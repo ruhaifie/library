65577c16de71a09ce9cd4f1be06f29ff1cf6ae3c
ruhaifie <ruhaifi_zainol@yahoo.com>
2020-12-07 15:33:11 +0800

    initial setup

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+//setup all the routes inside here
+const express = require('express'); //express variable
+const router = express.Router();//router variable
+
+//above: set rule
+//below use the set above
+
+router.get('/', (req, res) => { //req = request res = respond
+    //res.send('hello');
+    res.render('index'); //index.ejs file // use res.render to load up an ejs view file
+    //res.render() will look in a views folder for the view
+    
+}); 
+
+module.exports = router; //display router, export info *router from this file
---
+++ server.js
@@ -0,0 +1,40 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config(); //use config() instead of .parse()
+}
+//to load from .env file & import to process inside this server.js
+
+// load the things we need
+const express = require('express'); //to use express library
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index');  //file path to view index.js
+
+//above is set of rule/variable/path
+//below is: process use from rule from above
+//setup route here
+
+app.set('view engine', 'ejs');  // set the view engine to ejs
+app.set('views', __dirname + '/views'); //folder views
+app.set('layout', 'layouts/layout');    //folder views/layouts/layout.ejs
+
+app.use(expressLayouts);    //use from const expressLayouts
+app.use(express.static('public')); //folder public
+
+
+
+const mongoose = require('mongoose'); //to use mongoose library
+mongoose.connect(process.env.DATABASE_URL, {    //set up connection for database, URL for connection, connect to local mongo DB server, variable for database URL
+    useNewUrlParser: true, useUnifiedTopology: true });   //how to setup mongoDB
+const db = mongoose.connection; //setup log to use below
+db.on('error', error => console.error(error));  //print error
+db.once('open', () => console.log('Connected to Mongoose'));    //run db for first time
+
+//const url = 'mongodb://127.0.0.1:27017/mybrary';
+
+
+
+app.use('/', indexRouter);  //use indexRouter show the file path 
+
+//want app to listen to port, env = environment
+app.listen(process.env.PORT || 27017);   //node server, port 3030
